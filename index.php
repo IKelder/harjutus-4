d6f2d411e65b53cd28467ec3e71dda2a8d45e2da
Ingrid <Kelder>
2014-09-16 16:09:05 +0300

    Jagamine murd- ja taisarvudega

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<h2>Arvutamine murd- ja täisarvudega</h2>
+	<?php echo $float = 3.14; ?><br>
+	<?php echo $float + 7; ?> <br>
+	<?php echo 4/3 ; ?> <br>
+</body>
+</html>
